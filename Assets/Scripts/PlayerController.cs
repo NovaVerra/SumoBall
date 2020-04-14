5b777243361a6b12d5d4ffe49cbaeae504f440cf
lununglee <leo.lunung.lee@gmail.com>
2020-04-14 13:53:27 -0700

    added forward input - not relative to camera position

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField] float	MoveSpeed = 1000.0f;
+	Rigidbody	RB_Player;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Start is called before the first frame update
+	void	Start()
+	{
+		GetAllComponents();
+	}
+
+	void	GetAllComponents()
+	{
+		RB_Player = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void	Update()
+	{
+		InputManager();
+	}
+
+	void	InputManager()
+	{
+		ForwardHandler();
+	}
+
+	void	ForwardHandler()
+	{
+		float	ForwardInput = Input.GetAxis("Vertical");
+		RB_Player.AddForce(Vector3.forward * ForwardInput * MoveSpeed * Time.deltaTime);
+	}
 }
